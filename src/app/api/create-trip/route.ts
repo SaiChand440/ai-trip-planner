0bb445d29d529809150e82f442dfcf1b0a3d57ef
SaiChand440 <sai.chand@headout.com>
2024-03-15 23:27:55 +0530

    updated the prompt

--- src/app/api/create-trip/route.ts
+++ src/app/api/create-trip/route.ts
@@ -2,30 +2,45 @@ import * as openai from "../../../service/openai";
 import output_schema from "../../../tools/output_schema.json";
 import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
 
+interface IRequest {
+  destination: string;
+  date: {
+    from: Date;
+    to: Date;
+  };
+  usertype: "solo" | "couple" | "friends" | "family";
+  budget:
+    | "<500"
+    | "500-1000"
+    | "1000-2000"
+    | "2000-5000"
+    | "5000-10000"
+    | ">10000";
+}
+
 export async function POST(request: Request) {
-  const { inquiry } = await request.json();
+  const { destination, date:{from,to}, usertype, budget } : IRequest = await request.json();
 
-  console.log("inquiry",inquiry);
-
-  if (!inquiry) {
-    return new Response("Bad request", {
-      status: 400,
-    });
+  if (!destination || !from || !to || !usertype || !budget) {
+    return Response.json({success: false, error: "some of the fields are missing"},{
+        status: 400,
+        statusText: 'fail'
+    })
   }
 
   let system_prompt =
-    `You are a helpful travel planner specializing in ${inquiry} and designed to output json.\n` +
-    `Your output is to an API.\n` +
-    `Create valid json complying to the schema.\n\n`;
-
-  system_prompt +=
-    `# today\n` +
-    `Today is ${new Date()}.\n\n` +
-    `# json output schema\n` +
+    `You are a helpful travel planner specializing in ${destination} and designed to output json. ` +
+    `Your output is called as an API. ` +
+    `Create valid json complying to the schema. ` +
+    `Create an itinerary starting from ${from} and ending on ${to}, include activities for all days including the start and end date` +
+    `This is a ${usertype} trip. ` +
+    `The budget should be in the range of ${budget}` +
+    `and json output schema` +
     JSON.stringify(output_schema, null, 2);
 
   let messages: ChatCompletionMessageParam[] = [
     { role: "system", content: system_prompt },
+    { role: "user", content: `Create an itinerary to ${destination}` },
   ];
 
   const response = await openai.chat({
@@ -34,13 +49,14 @@ export async function POST(request: Request) {
 
   console.log("response", response.message);
 
-  const output = response.message.content;
+  const output = response.message.content
+    ?.replace(/\\/g, "")
+    .replace(/\n/g, "")!;
 
   return new Response(
     JSON.stringify({
       status: "ok",
-      text: "",
-      output,
+      data : JSON.parse(output),
     }),
     {
       status: 200,
