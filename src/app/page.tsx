6071d3b09bab50e8349c5ac0f92cf7ba288271e5
rishi320 <rishithagoud01@gmail.com>
2024-05-15 21:30:43 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { GridBackgroundDemo } from "@/components/customcomponents/GridBackGround";
 
 export default function Home() {
+  console.log("namaste dhora");
   return (
       <GridBackgroundDemo />
   );
