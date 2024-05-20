58fa1a9ef09ac56f9ea332c1761520ed22893a31
SaiChand440 <sai.chand@headout.com>
2024-05-20 19:23:36 +0530

    schema update

--- src/app/dbmodels/itinerarySchema.ts
+++ src/app/dbmodels/itinerarySchema.ts
@@ -1,7 +1,7 @@
-import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { jsonb, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
 export const itinerarySchema = pgTable("itinerary", {
   user_id: text("user_id"),
   created_at: timestamp("created_at"),
-  trip_data: text("trip_data"),
+  trip_data: jsonb("trip_data"),
 });
