0e5227a315294adcec998ed60c32e67143c0d908
SaiChand440 <59303987+SaiChand440@users.noreply.github.com>
2024-05-22 09:40:23 +0530

    updated to actions (#9)
    Co-authored-by: SaiChand440 <sai.chand@headout.com>

--- src/app/itinerary/components/Itinerary.tsx
+++ src/app/itinerary/components/Itinerary.tsx
@@ -5,20 +5,28 @@ import React from "react";
 
 interface IProps {
   data: any;
+  outputFromApi: boolean;
 }
 
-export default function Itinerary({ data }: IProps) {
+export const Itinerary = ({ data, outputFromApi }: IProps) => {
+  if (!data) {
+    return <div>Loading...</div>;
+  }
 
+  const requiredData = typeof data === "string" ? JSON.parse(data) : data;
   return (
-    <div className="h-max w-full" style={{ backgroundColor: 'black' }}>
+    <div className="h-max w-full" style={{ backgroundColor: "black" }}>
       <div className="h-screen z-50">
         <h1 className="font-bold text-neutral-200 text-4xl text-center z-50">
-          {data?.data?.title}
+          {requiredData?.title}
         </h1>
-        <div className="mt-5 md:mt-15 lg:mt-20" style={{ width: '100%' }}>
-          <HoverEffect items={data?.data?.itineraries}></HoverEffect>
+        <div className="mt-5 md:mt-15 lg:mt-20" style={{ width: "100%" }}>
+          <HoverEffect
+            items={requiredData?.itineraries}
+            outputFromApi={outputFromApi}
+          ></HoverEffect>
         </div>
       </div>
     </div>
   );
-}
+};
