85e1dacefbfdd7a92c40754693709adc8eca2335
Ligil <ligilvjames1996@gmail.com>
2024-05-28 19:28:03 +0530

    mytrips product card

--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -1,16 +1,29 @@
 "use client";
+import { ProductCard } from '@/components/ui/ProductCard';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react'
 
 export default function page() {
+  const { data } = useQuery({
+    queryKey: ["mytrips"],
+    queryFn: async () => {
+      return (
+        await fetch(`${window.location.origin}/api/my-trips`, {
+          method: "GET",
+        })
+      ).json();
+    },
+    refetchOnWindowFocus: false,
+  });
   return (
-    <>
-      <div className="h-[calc(100vh-5rem)] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.4] bg-dot-black/[0.4] flex items-center justify-center">
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <div className="container flex justify-center items-center w-full h-[calc(100vh-5rem)]">
-          {/* <HeroText /> */}
-          {/* <CardStack items={CARDS} /> */}
-        </div>
-      </div>
-    </>
+    <div className="w-full dark:bg-black bg-white  dark:bg-dot-white/[0.4] bg-dot-black/[0.4]" style={{ flexDirection: 'row', display: 'flex', flexWrap: 'wrap', gap: 50, justifyContent: 'center' }}>
+      {data?.data?.map((trip) =>
+        <ProductCard title={trip.trip_data.title
+        } description={trip.trip_data.welcome.text}
+          heroImage={trip.trip_data.welcome.image}
+          data={trip} />
+
+      )}
+    </div>
   );
 }
