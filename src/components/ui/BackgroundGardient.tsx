0f67ee6618b4a1d2ddd4c2020123223fe6b85046
SaiChand440 <sai.chand@headout.com>
2024-05-30 22:49:45 +0530

    remove gradient

--- src/components/ui/BackgroundGardient.tsx
+++ src/components/ui/BackgroundGardient.tsx
@@ -40,27 +40,6 @@ export const BackgroundGradient = ({
                     backgroundSize: animate ? "400% 400%" : undefined,
                 }}
                 className={cn(
-                    "absolute inset-0 rounded-3xl z-[1] opacity-60 group-hover:opacity-100 blur-xl  transition duration-500 will-change-transform",
-                    " bg-[radial-gradient(circle_farthest-side_at_0_100%,#00ccb1,transparent),radial-gradient(circle_farthest-side_at_100%_0,#7b61ff,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#ffc414,transparent),radial-gradient(circle_farthest-side_at_0_0,#1ca0fb,#141316)]"
-                )}
-            />
-            <motion.div
-                variants={animate ? variants : undefined}
-                initial={animate ? "initial" : undefined}
-                animate={animate ? "animate" : undefined}
-                transition={
-                    animate
-                        ? {
-                            duration: 5,
-                            repeat: Infinity,
-                            repeatType: "reverse",
-                        }
-                        : undefined
-                }
-                style={{
-                    backgroundSize: animate ? "400% 400%" : undefined,
-                }}
-                className={cn(
                     "absolute inset-0 rounded-3xl z-[1] will-change-transform",
                     "bg-[radial-gradient(circle_farthest-side_at_0_100%,#00ccb1,transparent),radial-gradient(circle_farthest-side_at_100%_0,#7b61ff,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#ffc414,transparent),radial-gradient(circle_farthest-side_at_0_0,#1ca0fb,#141316)]"
                 )}
