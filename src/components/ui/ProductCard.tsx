5d3fa42f911a449cf673f89282e76b919c8c5f53
SaiChand440 <sai.chand@headout.com>
2024-05-30 22:37:11 +0530

    route update and text clamp

--- src/components/ui/ProductCard.tsx
+++ src/components/ui/ProductCard.tsx
@@ -12,15 +12,25 @@ export function ProductCard({ data }) {
                 <Image
                     src={data.trip_data.welcome.image}
                     alt={data.trip_data.title}
-                    height="400"
-                    width="400"
-                    className="object-contain"
+                    height="300"
+                    width="300"
+                    className="object-cover rounded-[8px]"
                 />
-                <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+                <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200" style={{
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    whiteSpace: 'nowrap',
+                }}>
                     {data.trip_data.title}
                 </p>
 
-                <p className="text-sm text-neutral-600 dark:text-neutral-400" >
+                <p className="text-sm text-neutral-600 dark:text-neutral-400" style={{
+                    display: '-webkit-box',
+                    WebkitLineClamp: 3,
+                    WebkitBoxOrient: 'vertical',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                }}>
                     {data.trip_data.welcome.text.substring(0, 130)}...
                 </p>
                 {/* <button className="rounded-full text-white  space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800" style={{ paddingTop: 5, paddingBottom: 5, paddingLeft: 8, paddingRight: 8 }}>
@@ -28,10 +38,10 @@ export function ProductCard({ data }) {
             </button> */}
                 <div style={{ marginTop: 3 }}>
 
-                    <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white" >
+                    <span className="bg-zinc-700 rounded-[22px] text-[0.6rem] px-2 py-1 text-white" >
                         {data.trip_data.itineraries.length} days itinerary
                     </span>
-                    <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white ml-2">
+                    <span className="bg-zinc-700 rounded-[22px] text-[0.6rem] px-2 py-1 text-white ml-2">
                         $100
                     </span>
                 </div>
