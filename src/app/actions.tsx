de56c76797b8519478025bd31a69efde793b95d7
SaiChand440 <sai.chand@headout.com>
2024-05-30 14:20:55 +0530

    remove edge

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -5,10 +5,6 @@ import { createStreamableValue } from "ai/rsc";
 import outputSchema from "@/tools/outputSchema";
 import { google } from "@ai-sdk/google";
 
-export const config = {
-  runtime: "edge",
-};
-
 export async function generate(input: string) {
   "use server";
 
