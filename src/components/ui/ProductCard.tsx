a248fd58b1f4f211be7fdb3a05ce37ef529ab8e1
Ligil <ligilvjames1996@gmail.com>
2024-06-01 15:51:32 +0530

    changes style

--- src/components/ui/ProductCard.tsx
+++ src/components/ui/ProductCard.tsx
@@ -11,24 +11,25 @@ export function ProductCard({ data }) {
         return null;
     }
     return (
-        <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
-            <div style={{ height: 345, width: 300 }} onClick={() =>{
+        <BackgroundGradient className="rounded-[22px] bg-white dark:bg-zinc-900">
+            <div style={{ height: 378, padding: 5, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} onClick={() => {
                 route.push(`/itinerary/${encodeURIComponent(data.trip_id)}`);
             }}>
 
                 <Image
                     src={data.trip_data.welcome.image}
                     alt={data.trip_data.title}
-                    height="300"
-                    width="300"
-                    className="object-cover rounded-[8px]"
+                    height="200"
+                    width="350"
+                    className="rounded-[8px]"
+                    layout='fixed'
                 />
                 <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200" style={{
                     overflow: 'hidden',
                     textOverflow: 'ellipsis',
                     whiteSpace: 'nowrap',
                 }}>
-                    {data.trip_data.title}
+                    {data.trip_data.title.substring(0, 30)}..
                 </p>
 
                 <p className="text-sm text-neutral-600 dark:text-neutral-400" style={{
@@ -38,12 +39,12 @@ export function ProductCard({ data }) {
                     overflow: 'hidden',
                     textOverflow: 'ellipsis',
                 }}>
-                    {data.trip_data.welcome.text.substring(0, 130)}...
+                    {data.trip_data.welcome.text.substring(0, 120)}...
                 </p>
                 {/* <button className="rounded-full text-white  space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800" style={{ paddingTop: 5, paddingBottom: 5, paddingLeft: 8, paddingRight: 8 }}>
                 <span>Preview</span>
             </button> */}
-                <div style={{ marginTop: 3 }}>
+                <div style={{ marginTop: 1 }}>
 
                     <span className="bg-zinc-700 rounded-[22px] text-[0.6rem] px-2 py-1 text-white" >
                         {data.trip_data.itineraries.length} days itinerary
