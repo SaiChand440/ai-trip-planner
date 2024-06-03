7b568fb152040a61b9ca8749a2932799d3031108
SaiChand440 <sai.chand@headout.com>
2024-06-03 09:16:31 +0530

    build error

--- src/components/customcomponents/HomeSection2.tsx
+++ src/components/customcomponents/HomeSection2.tsx
@@ -6,7 +6,8 @@ export default function HomeSection2() {
     <div className="w-full h-auto md:h-[calc(100vh-5rem)] py-10 dark:bg-black bg-white">
       <div className="container mx-auto max-w-5xl lg:px-5">
         <h2 className="px-6 text-center text-3xl font-bold md:text-4xl lg:px-12 xl:text-5xl text-neutral-200 ">
-        Your <span className="text-indigo-300">AI Guide</span> to Perfect Trips
+          Your <span className="text-indigo-300">AI Guide</span> to Perfect
+          Trips
         </h2>
         <div className="flex flex-wrap mt-4">
           <div className="w-full p-4 md:p-10 sm:w-1/2 md:mt-8">
@@ -14,7 +15,7 @@ export default function HomeSection2() {
               The most simple
             </h3>
             <p className="text-gray-200">
-            Design your ideal itinerary with Hadana AI. Our sophisticated algorithms consider your chosen attractions, budget, and preferences to craft a personalized travel plan that's perfect for you.
+              {"Design your ideal itinerary with Hadana AI. Our sophisticated algorithms consider your chosen attractions, budget, and preferences to craft a personalized travel plan that's perfect for you."}
             </p>
           </div>
           <div className="w-full p-4 md:p-6 sm:w-1/2 flex justify-center">
@@ -23,29 +24,31 @@ export default function HomeSection2() {
               alt="temporary"
               width={"250"}
               height={"250"}
-            //   className=""
+              //   className=""
             />
           </div>
         </div>
         <div className="flex flex-wrap flex-col-reverse sm:flex-row mt-4">
-        <div className="w-full p-4 md:p-10 sm:w-1/2 flex justify-center">
+          <div className="w-full p-4 md:p-10 sm:w-1/2 flex justify-center">
             <Image
               src={"/images/temporary_logo.png"}
               alt="temporary"
               width={"250"}
               height={"250"}
-            //   className="-my-5"
+              //   className="-my-5"
             />
           </div>
           <div className="w-full p-4 md:p-6 sm:w-1/2 md:mt-8">
             <h3 className="mb-3 w-fit bg-primary-green/70 text-2xl font-bold leading-none text-white md:text-3xl">
-            Craft Your Adventure
+              Craft Your Adventure
             </h3>
             <p className="text-gray-200">
-            Discover your perfect adventure with Hadana AI, the ultimate travel companion powered by Generative AI. Simply input your preferences, from destination desires to budget constraints, and let Hadana AI work its magic.
+              Discover your perfect adventure with Hadana AI, the ultimate
+              travel companion powered by Generative AI. Simply input your
+              preferences, from destination desires to budget constraints, and
+              let Hadana AI work its magic.
             </p>
           </div>
-          
         </div>
       </div>
     </div>
