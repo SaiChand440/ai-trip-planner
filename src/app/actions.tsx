97b80a0480b5e56cb3ac77e1fe6331ccd469fa22
Ligil <ligilvjames1996@gmail.com>
2024-06-13 23:17:59 +0530

    use model based on days

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -20,11 +20,11 @@ export async function generate(input: z.infer<typeof formSchema>) {
 
   const stream = createStreamableValue();
   const status = createStreamableValue('in_progress');
+  const numberOfDays = Math.ceil((new Date(to).getTime() - new Date(from).getTime()) / (1000 * 60 * 60 * 24));
   (async () => {
     let system_prompt = `You are a helpful travel planner specialized in ${destination}. Create an itinerary starting from ${from} and ending on ${to}, including activities for all days along with start and end date. Remember to not include about arrival and departure. This is a ${usertype} trip. The total budget of the trip is ${budget}, split the budget into daily expenses based on the itinerary.`;
-
     const { partialObjectStream } = await streamObject({
-      model: google("models/gemini-1.5-flash-latest"),
+      model: numberOfDays > 4 ? google("models/gemini-1.5-flash-latest") : openai("gpt-4o"),
       // model: openai("gpt-4o"),
       schema: outputSchema,
       system: system_prompt,
