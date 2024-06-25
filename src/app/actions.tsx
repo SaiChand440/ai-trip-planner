d070ed085d45db2f3e8223a86d63e0cd2ac1d28c
SaiChand440 <sai.chand@headout.com>
2024-06-25 23:19:39 +0530

    update actions

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -20,12 +20,12 @@ export async function generate(input: z.infer<typeof formSchema>) {
   const stream = createStreamableValue();
   const status = createStreamableValue('in_progress');
   (async () => {
-    let system_prompt = `You are a helpful travel planner specialized in ${destination}. Create an itinerary starting from ${from} and ending on ${to}, including activities for all days along with start and end date. Remember to not include about arrival and departure. This is a ${usertype} trip. The total budget of the trip is ${budget}, split the budget into daily expenses based on the itinerary.`;
+    let system_prompt = `You are a helpful travel planner specialized in ${destination}. Create an itinerary starting from ${from} and ending on ${to} with a minimum of 400 characters for each day mandatorily,Try to include the specific address of each location only in the places key-value pair of the specified json output format. If specific start and end locations are not given, choose ones that you think are suitable and give specific addresses, be sure to mention the type of transit for the trip and also including activities for all days along with start and end date. Remember to not include about arrival and departure, no mention about arrival and departure from given destination whatsoever. This is a ${usertype} trip. The total budget of the trip is ${budget}, split the budget into daily expenses based on the itinerary.`;
     const { partialObjectStream } = await streamObject({
       model: openai("gpt-4o"),
       schema: outputSchema,
       system: system_prompt,
-      prompt: `Create an itinerary to ${destination} with minimum of 500 words for each day. The trip starts from ${from} and ends on ${to}. The total budget of the trip is ${budget}. The trip is for ${usertype}.`,
+      prompt: system_prompt,
       temperature: 0.4,
     });
 
