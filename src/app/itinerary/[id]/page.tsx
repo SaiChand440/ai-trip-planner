db9fed4d43a335f53f2413576adf0933990475d1
Ligil <ligilvjames1996@gmail.com>
2024-06-29 13:34:36 +0530

    map changes

--- src/app/itinerary/[id]/page.tsx
+++ src/app/itinerary/[id]/page.tsx
@@ -8,6 +8,7 @@ import { z } from "zod";
 import { Itinerary } from "../components/Itinerary";
 import { itineraryResponseSchema } from "@/components/customcomponents/TripPlanForm";
 import MapsComponent from "@/components/ui/MapComponent";
+import { cn } from "@/lib/utils";
 
 export const dynamic = "force-dynamic";
 export const maxDuration = 60;
@@ -70,15 +71,16 @@ export default function Page({ params }: { params: { id: string } }) {
       })();
     }
   }, [data]);
-
+  console.log("data",data)
+  console.log("responseData",responseData)
   return (
     !data ?
       (<div className="w-full h-auto dark:bg-black bg-white flex items-center justify-center ">
         <div className="loader"></div>
       </div>) :
       <>
-        <div className="w-[60%] h-auto dark:bg-black bg-white flex items-center justify-center ">
-          <div className="flex justify-start items-center w-full flex-col dark:bg-black bg-white">
+      <div className={cn("w-[60%] h-auto dark:bg-black bg-white flex items-center justify-center")}>
+      <div className="flex justify-start items-center w-full flex-col dark:bg-black bg-white">
             <Itinerary
               data={data?.trip_data ?? responseData?.data ?? itineraryData}
               outputFromApi={
@@ -90,9 +92,12 @@ export default function Page({ params }: { params: { id: string } }) {
               }
             />
           </div>
-          <div className="w-[40%]  h-screen flex flex-1" >
-            <MapsComponent />
-          </div>
+       
+          { (data?.trip_data?.destination?.location ?? responseData?.trip_data?.destination?.location) && 
+                  <div className="w-[40%]  h-screen flex flex-1" >
+                  <MapsComponent data={data ?? responseData  }/>
+                </div>}
+      
         </div >
       </>
   );
--- src/components/ui/MapComponent.tsx
+++ src/components/ui/MapComponent.tsx
@@ -1,76 +1,147 @@
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, InfoWindow, Marker, useJsApiLoader } from '@react-google-maps/api';
 import React, { useEffect, useState } from 'react'
-import {
-    setKey,
-    setDefaults,
-    setLanguage,
-    setRegion,
-    fromAddress,
-    fromLatLng,
-    fromPlaceId,
-    setLocationType,
-    geocode,
-    RequestType,
-} from "react-geocode";
-type Props = {}
+import { itineraryResponseSchema } from '../customcomponents/TripPlanForm';
+type Props = { data: {} }
 
-export default function MapsComponent({ }: Props) {
+export default function MapsComponent({ data }: Props) {
     const mapsKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? ''
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
         googleMapsApiKey: mapsKey
     })
     const [map, setMap] = React.useState(null)
-    const onLoad = React.useCallback(function callback(map) {
-        // This is just an example of getting and using the map instance!!! don't just blindly copy!
-        const bounds = new window.google.maps.LatLngBounds(center);
+
+    const onLoad = (map) => {
+        const bounds = new google.maps.LatLngBounds();
+        // markers.forEach(({ position }) => bounds.extend(center));
+        // markers.forEach(({ position }) => bounds.extend(center));
+
+        bounds.extend(center)
         map.fitBounds(bounds);
-        console.log(map)
+
+        // google.maps.event.addListener(map, 'bounds_changed', function (event) {
+        //     google.maps.event.removeListener(zoomChangeBoundsListener);
+        // });
+        var mapZoom = null;
+        google.maps.event.addListenerOnce(map, 'bounds_changed', function () {
+            if (mapZoom != map.getZoom()) {
+                mapZoom = (15);
+                map.setZoom(mapZoom);
+            }
+        });
         setMap(map)
-    }, [])
+        // if (!bounds.isEmpty()) {
+        //     var originalMaxZoom = map.maxZoom;
+        //     map.setOptions({ maxZoom: 18 });
+        //     map.setOptions({ maxZoom: originalMaxZoom });
+        // }
+    };
 
     const onUnmount = React.useCallback(function callback(map) {
         setMap(null)
     }, [])
     const containerStyle = {
-        width: '100%',
-        height: '100%',
+        width: '90%',
+        height: '110%',
     };
 
     const center = {
-        lat: -3.745,
-        lng: -38.523
+        lat: data?.trip_data?.destination?.location.lat,
+        lng: data?.trip_data?.destination?.location.lng
     }
-    // console.log("item", items)
-    // setDefaults({
-    //   key: mapsKey,
-    //   language: "en",
-    // });
+    const [locationdata, setLocationdata] = useState([])
+    useEffect(() => {
+        const tempData = []
+        data.trip_data?.itineraries.map((item) => tempData.push(item.places))
+        const filteredData = tempData.map((item, index) => {
+            return item.filter((item) => typeof item !== "string")
+        })
+        setLocationdata(filteredData)
+        console.log("trip data", filteredData)
+    }, [])
+
     const [latLong, setLatLng] = useState({ lat: 0, lng: 0 })
-    // setKey(mapsKey);
-    // setLanguage("en");
-    // useEffect(() => {
-    //     fromAddress("Eiffel Tower")
-    //         .then(({ results }) => {
-    //             const { lat, lng } = results[0].geometry.location;
-    //             setLatLng({ lat, lng })
-    //             console.log("ojoejfojejf", lat, lng, map);
-    //         })
-    //         .catch(console.error);
-    // }, [])
+
+    const defaultMapOptions = {
+        styles: obj
+    };
+    const [activeMarker, setActiveMarker] = useState(null);
+
+    const handleActiveMarker = (marker) => {
+        console.log("mar", marker)
+        if (marker === activeMarker) {
+            return;
+        }
+        setActiveMarker(marker);
+    };
     return (
         isLoaded &&
 
-        <div className='fixed flex w-[40%] h-[100%]'>
+        <div style={{
+            position: 'fixed', display: 'flex', width: '40%',
+            height: '90%',
+        }}>
             <GoogleMap
                 mapContainerStyle={containerStyle}
                 center={center}
-                zoom={5}
                 onLoad={onLoad}
                 onUnmount={onUnmount}
-            />
+                options={defaultMapOptions}
+
+            >
+                {/* {locationdata.map(({ id, name, position }) => (
+                    <Marker
+                        key={id}
+                        position={center}
+                        onClick={() => handleActiveMarker(id)}
+                    >
+                        {activeMarker === id ? (
+                            <InfoWindow onCloseClick={() => setActiveMarker(null)}>
+                                <div style={{ color: 'red' }}>{id}</div>
+                            </InfoWindow>
+                        ) : null}
+                    </Marker>
+                ))} */}
+                {locationdata.map((i, index) => i.map(({ location, place }) =>
+
+                    <Marker
+                        label={index + 1}
+                        title='dwd'
+                        key={location.lat}
+                        position={location}
+                        onClick={() => handleActiveMarker(location.lat)}
+                    >
+                        {activeMarker === location.lat ? (
+                            <InfoWindow onCloseClick={() => setActiveMarker(null)}>
+                                <div style={{ color: 'red' }}>{place}</div>
+                            </InfoWindow>
+                        ) : null}
+                    </Marker>)
+                )}
+            </GoogleMap>
         </div>
 
 
     )
 }
+
+const obj = [
+    {
+        "featureType": "all",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    }
+]
