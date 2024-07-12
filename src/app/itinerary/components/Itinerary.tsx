db2d3eddc30e63661d9499ec1434e80f8e21fd87
rishi320 <rishithagoud01@gmail.com>
2024-07-12 22:31:09 +0530

    pdf and share added

--- src/app/itinerary/components/Itinerary.tsx
+++ src/app/itinerary/components/Itinerary.tsx
@@ -2,8 +2,10 @@ import { HoverEffect } from "@/components/ui/HoverEffect";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { Share2, ArrowDownToLine } from "lucide-react";
 import React from "react";
 import {generatePdf} from '@/tools/pdfGenerator';
+import { cn } from "@/lib/utils";
 
 interface IProps {
   data: any;
@@ -22,6 +24,16 @@ export const Itinerary = ({ data, outputFromApi, dates }: IProps) => {
       generatePdf(requiredData, dates)
     }
   }
+
+  const copyToClipboard = async () => {
+    const link = window.location.href;
+    try {
+      await navigator.clipboard.writeText(link);
+      console.log("Link copied to clipboard");
+    } catch (err) {
+      console.error("Failed to copy link: ", err);
+    }
+  };
   return (
     <div className="h-max w-full">
       <div className="h-screen">
@@ -32,17 +44,27 @@ export const Itinerary = ({ data, outputFromApi, dates }: IProps) => {
           }}
         >
           <div
-            className="h-[100%] w-[100%] py-10 md:py-32 bg-gradient-to-b from-[rgba(0,0,0,0.5)] via-[rgba(0,0,0,0)] via-30% to-[rgba(0,0,0,0.7)] transition-colors"
+            className="h-[100%] w-[100%] mx-auto py-10 md:py-32 bg-gradient-to-b from-[rgba(0,0,0,0.5)] via-[rgba(0,0,0,0)] via-30% to-[rgba(0,0,0,0.7)] transition-colors"
             style={{
               backgroundColor: "rgba(0, 0, 0, 0.2)",
             }}
           >
+            <div className="flex flex-col md:flex-row gap-4 items-center justify-center">
             {requiredData?.title}
+            <div className="flex">
+           
+            <Button id="share" variant={'link'} onClick={copyToClipboard}>
+            <Share2 className={cn("h-8 w-8")}/>
+            </Button>
+            <Button id="download_pdf" variant={'link'} onClick={downloadPDF}>
+            <ArrowDownToLine className={cn("h-8 w-8")}/>
+            </Button>
+            </div>
+            </div>
           </div>
         </div>
         <div>
-          <Button>Share</Button>
-          <Button onClick={downloadPDF}>Save as PDF</Button>
+          
         </div>
         <div className="pt-5 md:pt-16 w-[100%] dark:bg-black bg-white">
           <HoverEffect
